7974e049f6e41dfac17d143237d642ef25128519
chen24833886 <chen24833886@gmail.com>
2018-04-20 11:50:51 +0800

    拉一個CUBE當砲台，寫C#去控制左右轉動

---
+++ Assets/plarer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plarer : MonoBehaviour {
+    public float speed;
+    
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float y = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+
+        transform.Rotate(new Vector3(0, y, 0));
+	}
+}
